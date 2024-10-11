3a8eab6ce0b1c568f15004340816f71c4f2aa8c4
Bruno_Ss <brunosch250@gmail.com>
2024-10-11 19:28:57 -0300

    Update

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -26,7 +26,7 @@ class SeriesController extends Controller
      */
     public function create()
     {
-        //
+        return view('series.create');
     }
 
     /**
---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,6 @@
+<x-layout title="Nova Série">
+    <form action="" method="">
+        <label for="nome">None:</label>
+        <input type="text" id="nome" name="nome">
+    </form>
+</x-layout>
